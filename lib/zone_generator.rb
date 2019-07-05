adb0ec059d83cf4e98daa54c6d6d37766c01ec2f
Dominik Menke <dom@digineo.de>
2019-07-05 20:28:40 +0200

    handle paths with Pathname

--- lib/zone_generator.rb
+++ lib/zone_generator.rb
@@ -1,60 +1,75 @@
+require "pathname"
+
 class ZoneGenerator
+  def self.run(basedir)
+    zg = new basedir
+    zg.generate
+    zg.deploy
+  end
+
   def initialize(basedir)
-    @generated    = "#{basedir}/tmp/generated"
-    @workspace    = "#{basedir}/tmp/cache"
-    @zones_dir    = "#{@workspace}/zones"
-    @template_dir = "#{@workspace}/templates"
+    basedir = Pathname.new(basedir)
 
-    @tmp_named = "#{@generated}/named.conf"
-    @tmp_zones = "#{@generated}/zones"
+    @generated    = basedir.join "tmp/generated"
+    @workspace    = basedir.join "tmp/cache"
+    @zones_dir    = @workspace.join "zones"
+    @template_dir = @workspace.join "templates"
 
-    @config = YAML.load_file("#{@workspace}/config.yaml")
-    @config.deep_symbolize_keys!
+    @tmp_named = @generated.join "named.conf"
+    @tmp_zones = @generated.join "zones"
+
+    config = YAML.load @workspace.join("config.yaml").read
+    config.deep_symbolize_keys!
     @soa = {
-      origin: "@",
-      ttl: "86400",
-      primary: "example.com.",
-      email: "hostmaster@example.com",
-      refresh: "8H",
-      retry: "2H",
-      expire: "1W",
+      origin:     "@",
+      ttl:        "86400",
+      primary:    "example.com.",
+      email:      "hostmaster@example.com",
+      refresh:    "8H",
+      retry:      "2H",
+      expire:     "1W",
       minimumTTL: "11h"
-    }.merge(@config[:soa])
+    }.merge(config[:soa])
 
     # Rewrite email address
     if (email = @soa[:email]).include?("@")
-      @soa[:email] = email.sub("@",".") << "."
+      @soa[:email] = email.sub("@", ".") << "."
     end
 
-    FileUtils.rm_rf   @generated # Tote Zonen-Definitionen brauchen wir nicht.
-    FileUtils.mkdir_p @generated
+    @pdns_named_conf = Pathname.new config[:named_conf]
+    @pdns_zones_dir = Pathname.new config[:zones_dir]
+    @after_deploy = config[:execute]
+
+    # we don't want dead zone definitions
+    @generated.rmtree if @generated.exist?
+    @generated.mkpath
   end
 
   # Generates all zones
   def generate
-    File.open(@tmp_named,"w") do |f|
-      Dir.glob("#{@zones_dir}/**/*.rb").sort.each do |file|
-        domain = File.basename(file).sub(/\.rb$/,"")
+    @tmp_named.open("w") do |f|
+      Pathname.glob(@zones_dir.join("**/*.rb")).sort.each do |file|
+        domain = File.basename(file).sub(/\.rb$/, "")
         generate_zone(file, domain)
 
-        f.puts %Q<zone "#{domain}" IN { type master; file "#{@config[:zones_dir]}/#{domain}"; };>
+        f.puts %Q<zone "#{domain}" IN { type master; file "#{@pdns_zones_dir}/#{domain}"; };>
       end
     end
   end
 
   # Generates a single zone file
   def generate_zone(file, domain)
-    zone = Zone.new(domain, @template_dir, @soa)
-    zone.send :eval_file, file
+    zone = Zone.new(domain, @template_dir.to_s, @soa)
+    zone.send :eval_file, file.to_s
     new_zonefile = zone.zonefile
 
     # path to the deployed version
-    old_file = "#{@config[:zones_dir]}/#{domain}"
+    old_file = Pathname.new(@pdns_zones_dir).join(domain)
 
     # is there already a deployed version?
-    if File.exists?(old_file)
+    if old_file.exist?
       # parse the deployed version
-      old_output   = File.read(old_file)
+      old_output   = old_file.read
       old_zonefile = Zonefile.new(old_output)
       new_zonefile.soa[:serial] = old_zonefile.soa[:serial]
 
@@ -76,23 +91,24 @@ class ZoneGenerator
     end
 
     # Write new zonefile
-    output_file_path = "#{@tmp_zones}/#{domain}"
-    FileUtils.mkdir_p File.dirname(output_file_path)
-    File.open(output_file_path, "w"){|f| f.write new_output }
+    output_file_path = @tmp_zones.join(domain)
+    output_file_path.dirname.mkpath
+    output_file_path.open("w") {|f| f.write new_output }
   end
 
   def deploy
     # Remove zones directory
-    FileUtils.rm_rf @config[:zones_dir]
+    @pdns_zones_dir.rmtree
 
-    FileUtils.copy       @tmp_named, @config[:named_conf]
-    FileUtils.copy_entry @tmp_zones, @config[:zones_dir]
+    FileUtils.copy       @tmp_named, @pdns_named_conf
+    FileUtils.copy_entry @tmp_zones, @pdns_zones_dir
 
-    cmd = @config[:execute]
-    print "Executing '#{cmd}' ... "
-    out = `#{cmd}`
-    puts "done"
+    if cmd = @after_deploy
+      print "Executing '#{cmd}' ... "
+      out = `#{cmd}`
+      puts "done"
 
-    raise out if $?.to_i != 0
+      raise out if $?.to_i != 0
+    end
   end
 end
